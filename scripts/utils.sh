9f23e97dd10bb6d6a6c400a86d9325bd6e1ea211
Donnie Mattingly <donniewmattingly@gmail.com>
2019-04-22 22:17:52 -0400

    convert deploy script to build node project

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -2,8 +2,12 @@
 
 function deploy_lambda(){
     local FUNCTION_NAME=$1
+    rm -rf dist
     mkdir -p dist
-    zip -r dist/${FUNCTION_NAME}.zip index.js node_modules
+    npm run build
+    cd dist
+    zip -r ${FUNCTION_NAME}.zip . ../node_modules
+    cd ..
     aws s3 cp dist/${FUNCTION_NAME}.zip s3://parlicious-functions
     aws lambda update-function-code --function-name ${FUNCTION_NAME} --s3-bucket parlicious-functions --s3-key ${FUNCTION_NAME}.zip
     aws lambda publish-version --function-name ${FUNCTION_NAME}
